51e878cdad0df498d51d9e1ffd70d2d0f0398ee7
yancmrg <132528533+yancmrg@users.noreply.github.com>
2024-10-25 20:56:15 -0300

    Projeto versão final

---
+++ ProjetoAlura_5/perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma função?',
+    'Uma função é uma relação entre dois conjuntos numéricos A e B, de modo que para todo elemento de A existe um, e somente um, correspondente em B'
+)
+
+criaCartao(
+    'Química',
+    'Quais são os elementos da Tabela Periódica conhecidos por serem Gases Nobres',
+    'He, Ne, Ar, Kr, Xe, Rn - Hélio, Neônio, Argônio, Kriptônio, Xenônio e Radônio'
+)
+
+criaCartao(
+    'História',
+    'Quais os Países eram os principais envolvidos na segunda guerra mundia?',
+    'Eixo - Alemanha, Itália e Japão / Aliados - Estados Unidos, Inglaterra e França'
+)
